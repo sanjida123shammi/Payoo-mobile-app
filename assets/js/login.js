b50aa5dfd8fb19da02ce2c9cd4f9fe03993c7afa
Sanjida Shammi <sanjida717270@gmail.com>
2025-07-22 10:22:06 +0600

    first commit

---
+++ assets/js/login.js
@@ -0,0 +1,32 @@
+// Login Button
+
+// Step-1: Set event handler
+// document.getElementById('btn-login')
+//     .addEventListener('click', function(event){
+//         // Step-2: Prevent default behavior (prevent reloading browser)
+//         event.preventDefault();   // --Vejal to beginners
+
+//         // Step-3: Get the phone number
+//         const phoneNumber = document.getElementById('phone-number').value;
+//         const pinNumber = document.getElementById('pin-number').value;
+
+//         // This is a temporary method. In future we will never try this method
+//         if (phoneNumber === '01720724292' && pinNumber === '15973') {
+//             console.log('Congrats, you are logged in');
+//         }
+//         else{ 
+//             alert('Invalid User');
+//         }
+//     })
+
+document.getElementById('btn-login')
+    .addEventListener('click', function(event){
+        event.preventDefault();
+
+        const phoneNumber = document.getElementById('phone-number').value;
+        const pinNumber = document.getElementById('pin-number').value;
+
+        if (phoneNumber === '01715724292' && pinNumber === '15973') {
+            window.location.href = '/home.html'
+        }
+    })
---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} **/
+module.exports = {
+    theme: {
+        extend: {
+            colors: {
+                clifford: '#da373d',
+                // 'primary-color': '#080808',
+                // 'secondary-color': '#0874F2',
+            },
+        },
+    },
+    plugins: [],
+};
